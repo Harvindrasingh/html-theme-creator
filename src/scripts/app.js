64f74572ad77407a103f94bd19786e30e209262d
Kulwinder Kohli <info@iamkulwinderkohli.com>
2018-10-16 22:58:40 +0530

    - code

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,4 +1,10 @@
 @import "jquery.min.js";
 @import "popper.js";
 @import "bootstrap.min.js";
+@import "../../node_modules/fullpage.js/vendors/easings.js";
+@import "../../node_modules/fullpage.js/vendors/scrolloverflow.js";
+@import "../../node_modules/fullpage.js/dist/fullpage.js";
+@import "../../node_modules/particles.js/particles.js";
+@import "jquery.sticky.js";
+@import "letterfx.js";
 @import "custom.js";
