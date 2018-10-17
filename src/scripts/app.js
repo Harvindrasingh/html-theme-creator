8a1b5ec319165722d416c9d4cc5c4b9b86ca6ba1
Kulwinder Kohli <info@iamkulwinderkohli.com>
2018-10-17 10:39:53 +0530

    - two column template

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,8 +3,8 @@
 @import "bootstrap.min.js";
 @import "../../node_modules/fullpage.js/vendors/easings.js";
 @import "../../node_modules/fullpage.js/vendors/scrolloverflow.js";
-@import "../../node_modules/fullpage.js/dist/fullpage.js";
-@import "../../node_modules/particles.js/particles.js";
+// @import "../../node_modules/fullpage.js/dist/fullpage.js";
+// @import "../../node_modules/particles.js/particles.js";
 @import "jquery.sticky.js";
 @import "letterfx.js";
 @import "custom.js";
