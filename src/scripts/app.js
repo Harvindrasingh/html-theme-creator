03b4fc60ebd2e12b361fddbcc4123e2f968c4aae
Kulwinder Kohli <info@iamkulwinderkohli.com>
2018-11-06 22:34:47 +0530

    - project init

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,10 +1,12 @@
 @import "jquery.min.js";
 @import "popper.js";
 @import "bootstrap.min.js";
-@import "../../node_modules/fullpage.js/vendors/easings.js";
-@import "../../node_modules/fullpage.js/vendors/scrolloverflow.js";
+// @import "../../node_modules/fullpage.js/vendors/easings.js";
+// @import "../../node_modules/fullpage.js/vendors/scrolloverflow.js";
 // @import "../../node_modules/fullpage.js/dist/fullpage.js";
 // @import "../../node_modules/particles.js/particles.js";
-@import "jquery.sticky.js";
+// @import "jquery.sticky.js";
 @import "letterfx.js";
+@import "bouncy.js";
+@import "slick.js";
 @import "custom.js";
--- src/scripts/custom.js
+++ src/scripts/custom.js
@@ -3,9 +3,27 @@ $(function() {
     // Fullpage scrolling
     // $('#fullpage').fullpage();
     
-	// $(".about__me-detail").letterfx({
-	// 	fx: "smear",
-	// 	letter_end: "stay",
-	// 	fx_duration: "3s"
-	// });
+	$(".description").letterfx({
+		fx: "smear",
+		letter_end: "stay",
+		fx_duration: "6s"
+	});
+
+	$(".variable").slick({
+		dots: false,
+		infinite: false,
+		slidesToShow: 3.2,
+		slidesToScroll: 1,  
+		autoplay: true,
+  		autoplaySpeed: 2000,
+		responsive: [
+			{
+		      breakpoint: 480,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1,
+		      }
+		    }
+	    ]
+	});
 });
